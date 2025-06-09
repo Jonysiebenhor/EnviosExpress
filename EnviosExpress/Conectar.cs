c64bd7e73aff2077dbd2b478061a8a6a34011de7
erickl2002 <el467989@gmail.com>
2025-06-09 09:24:19 -0600

    Pequeños cambios en el backend y en el procedimiento almacenado

--- EnviosExpress/Conectar.cs
+++ EnviosExpress/Conectar.cs
@@ -18,9 +18,8 @@ namespace EnviosExpress
         SqlConnection conexion = new SqlConnection();
 
         //Esta cadena de conexión la cambié por la de mi máquina local, esta se debe cambiar después por la cadena de conexión del servidor.
-        //String conexionString = "Data Source=PC-ERICK\\SQLEXPRESS;Initial Catalog=db_prueba;User ID=(Usuario de la BD);Password=(Contraseña de la BD);TrustServerCertificate=True;";
-        String conexionString = "Data Source=PC-ERICK\\SQLEXPRESS;Initial Catalog=db_prueba;User ID=(usuario de la bd);Password=(contraseña de la bd);TrustServerCertificate=True;";
-
+        String conexionString = "Data Source=PC-ERICK\\SQLEXPRESS;Initial Catalog=db_prueba;User ID=(Usuario de la BD);Password=(Contraseña de la BD);TrustServerCertificate=True;";
+       
 
         //¡OJO! LEER EL COMENTARIO DE ARRIBA!!
 
@@ -28,7 +27,7 @@ namespace EnviosExpress
 
         //String conexionString = "Data Source=DESKTOP-QTSGBLO;DATABASE=EnviosExpress;Integrated security=true";pasada
         //String conexionString = "Data Source=DESKTOP-KNTJ3BG\\SQLEXPRESS;DATABASE=EnviosExpress;Integrated security=true";
-        String conexionString = "workstation id = EnviosExpress.mssql.somee.com; packet size = 4096; user id = EnviosExpress; pwd=Envios3228@;data source = EnviosExpress.mssql.somee.com;";
+        //String conexionString = "workstation id = EnviosExpress.mssql.somee.com; packet size = 4096; user id = EnviosExpress; pwd=Envios3228@;data source = EnviosExpress.mssql.somee.com;";
         public void conectar()
         {
             try
